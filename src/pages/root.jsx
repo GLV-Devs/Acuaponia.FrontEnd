65643296eabba611c362b1ffa07ff379cc8587ab
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-10-07 22:37:15 -0400

    root

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+
 const Root = () => {
+
+    const navigate = useNavigate()
+    useEffect(() => {
+        navigate('/Login')
+    }, [])
+
     return(
         <div className="root">
-            
+            <Outlet/>
         </div>
     )
 }
