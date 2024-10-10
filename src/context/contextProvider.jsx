8a820af54e0b07a21feadca6dae07ed80b60d244
MaferVil <maria.viloria200329@gmail.com>
2024-10-09 22:38:35 -0400

    cliente(otra vez)

--- src/context/contextProvider.jsx
+++ src/context/contextProvider.jsx
@@ -1,10 +1,14 @@
 import { appContext } from './appContext'
 import { useState } from 'react'
+import { mainServerClient } from '../client/client'
 
 const AppContextProvider = ({children}) => {
 
+    const mainServerClient = new mainServerClient(loginData, serverUrl, httpErrorHandler)
+
     return(
         <appContext.Provider value={{
+            mainServerClient
 
         }}>
             {children}
