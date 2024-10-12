77d76e5309952f2b76d33efd2de81764ea5c4531
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-10-12 14:24:05 -0400

    Pagina de perfil avanzada

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,8 +1,37 @@
+import { useContext, useEffect } from "react"
+import { appContext } from '../context/appContext'
+import { getAccount } from "../client/ClientePrueba"
+
 const Profile = () => {
+
+    const {userData, setUserData, subServers} = useContext(appContext)
+
+    const getUserInfo = async () => {
+        let res = await getAccount()
+        setUserData(res.data.data[0])
+    }
+
+    useEffect(() => {
+        getUserInfo()
+    }, [])
+
     return(
-        <>
-        
-        </>
+        <div className='Profile'>
+            <h1>Bienvenido</h1>
+            <h1>{userData.realName}</h1>
+
+            <div className='card'>
+                <img src="" alt=''/>
+            </div>
+
+            <div className='ReportesSection'>
+
+            </div>
+
+            <div className='SubServersSection'>
+
+            </div>
+        </div>
     )
 }
 
