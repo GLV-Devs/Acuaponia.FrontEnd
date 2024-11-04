6bc0fdbb93264ed00c8fd68ed44da39032eb9e54
MaferVil <maria.viloria200329@gmail.com>
2024-11-04 00:25:45 -0400

    mas permisos pipipi

--- src/pages/userCreation.jsx
+++ src/pages/userCreation.jsx
@@ -1,133 +1,70 @@
-import { Form, Input, Button, message } from 'antd'
-import { useContext, useState } from 'react'
+import { Button, message, Steps, theme } from 'antd';
 import { useNavigate } from 'react-router-dom'
-import { postCreateAccount } from '../client/ClientePrueba'
-import { LeftOutlined } from '@ant-design/icons'
-import { backButtonStyle } from '../AntDIconStyles'
+import UserInfoCreation from '../components/UserInfoCreation'
+import UserPermissionCreation from '../components/UserPermissionCreation'
+import React, { useState } from 'react';
+
+const steps = [
+    {
+        title: 'Informacion de Usuario',
+        content: <UserInfoCreation/>,
+    },
+    {
+        title: 'Permisos',
+        content: <UserPermissionCreation/>,
+    },
+];
 
 const UserCreation = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate()    
 
-    const onSubmit = async () => {
-        let realName = document.getElementById('realName').value
-        let userName = document.getElementById('userName').value
-        let email = document.getElementById('email').value
-        let phoneNumber = document.getElementById('phoneNumber').value
-        let password = document.getElementById('password').value
+    const [current, setCurrent] = useState(0);
+    
 
-        const data = {
-            realName: realName,
-            userName: userName,
-            email: email,
-            phoneNumber: phoneNumber,
-            password: password,
-        }
-        console.log(data)
-        let res = await postCreateAccount(data)
-        console.log(res)
-        if(res.status == 200){
-            message.success('Usuario creado exitosamente')
-            navigate('/Dashboard')
-        }else if(res.status == 403){
-            message.error('El usuario ya existe')
-        }else{
-            message.error('Error del servidor')
-        }
-    }
+    const next = () => {
+        setCurrent(current + 1);
+    };
+    const prev = () => {
+        setCurrent(current - 1);
+    };
+    const items = steps.map((item) => ({
+        key: item.title,
+        title: item.title,
+    }));
 
-
-    return(
+        return(
         <div className='moduleCreation'>
-            <Form className='creationForm'
-                variant='filled'
-                size='small'
-                layout='vertical'
-            >
-                <div className='upperBar'>
-                    <LeftOutlined style={backButtonStyle} onClick={() => {navigate ('/Dashboard')}}/>
-                    <h1>Creacion de Usuario</h1>
+            <div className='steps'>
+            <Steps current={current} items={items} />
+                <div>{steps[current].content}</div>
+                <div
+                    style={{
+                    marginTop: 10,
+                    justifySelf: 'center',
+                    }}
+                >
+                    {current < steps.length - 1 && (
+                    <Button type="primary" onClick={() => next()}>
+                        Siguiente
+                    </Button>
+                    )}
+                    {current > 0 && (
+                    <Button
+                        style={{
+                        margin: '0 8px',
+                        }}
+                        onClick={() => prev()}
+                    >
+                        Anterior
+                    </Button>
+                    )}
+                    {current === steps.length - 1 && (
+                    <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                        Crear
+                    </Button>
+                    )}
                 </div>
-
-                <div className='form'>
-                    <Form.Item
-                        className='labels'
-                        label='Nombre'
-                        name='realName'
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Por favor ingrese su nombre',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Nombre..."/>
-                    </Form.Item>
-
-                    <Form.Item
-                        className='labels'
-                        label='Usuario'
-                        name='userName'
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Por favor ingrese su usuario',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Usuario..."/>
-                    </Form.Item>
-
-                    <Form.Item
-                        className='labels'
-                        label='Correo'
-                        name='email'
-                        rules={[
-                            {
-                                type: 'email',
-                                required: true,
-                                message: 'Por favor ingrese su correo',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Correo..."/>
-                    </Form.Item>
-
-                    <Form.Item
-                        className='labels'
-                        label='Telefono'
-                        name='phoneNumber'
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Por favor ingrese su numero de telefono',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Numero de telefono..."/>
-                    </Form.Item>
-
-                    <Form.Item
-                        className='labels'
-                        label='Contraseña'
-                        name='password'
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Por favor ingrese su contraseña',
-                            },
-                        ]}
-                    >
-                        <Input.Password className='password' placeholder='     Contraseña...' />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button className='formButton' htmlType="submit" type="primary" onClick={onSubmit}>
-                            CREAR
-                        </Button>
-                    </Form.Item>
-
-                </div>
-            </Form>
-        
+            </div>        
         </div>
     )
 }
