c6fc4cf8bb4c938fbe67db0156a9df179dc6b060
MaferVil <maria.viloria200329@gmail.com>
2024-11-09 01:06:55 -0400

    estilos SubServerSession

--- src/pages/SubServersSessions.jsx
+++ src/pages/SubServersSessions.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from 'react'
 import { getSubServerSessions, aproveSubServerSession, rejectSubServerSession } from '../client/ClientePrueba'
 import { Button } from 'antd'
 import { appContext } from '../context/appContext'
+import { AppstoreAddOutlined } from '@ant-design/icons'
 
 const SubServerSessions = () => {
 
@@ -65,18 +66,23 @@ const SubServerSessions = () => {
             <h1>Sesiones de subServidores</h1>
             { sessionsList.map(item => (
                 <div className='listItem' key={item.id}>
-                    <h2>{ item.givenName ? (item.givenName):(item.reportedName) }</h2>
-                    <h3>Primera conexion: {Date(item.firstRequestedDate).toString()}</h3>
-                    <h3>Ultima conexion: {Date(item.lastLoginDate).toString()}</h3>
-                    <h3>MAC: {item.requestingClientReportedMACAddress}</h3>
-                    { item.isApproved == null && (
-                        <>
-                            <Button onClick={() => aproveSubServer(item.id, item.requestingClientIP)}>Aprobar</Button>
-                            <Button onClick={() => rejectSubServer(item.id, item.requestingClientIP)}>rechazar</Button>
-                        </>
-                    ) }
-                    { item.isApproved == true && <h3>Sub servidor aprovado</h3> }
-                    { item.isApproved == false && <h3>Sub servidor rechazado</h3> }
+                    <div className='left'>
+                        <AppstoreAddOutlined style={{color:'#e95cff', fontSize:'60px'}}/>
+                    </div>
+                    <div className='info'>
+                        <h2>{ item.givenName ? (item.givenName):(item.reportedName) }</h2>
+                        <h3>Primera conexion: {Date(item.firstRequestedDate).toString()}</h3>
+                        <h3>Ultima conexion: {Date(item.lastLoginDate).toString()}</h3>
+                        <h3>MAC: {item.requestingClientReportedMACAddress}</h3>
+                        { item.isApproved == null && (
+                            <>
+                                <Button onClick={() => aproveSubServer(item.id, item.requestingClientIP)}>Aprobar</Button>
+                                <Button onClick={() => rejectSubServer(item.id, item.requestingClientIP)}>rechazar</Button>
+                            </>
+                        ) }
+                        { item.isApproved == true && <h3 className='approve'>Sub servidor aprovado</h3> }
+                        { item.isApproved == false && <h3 className='rejected'>Sub servidor rechazado</h3> }
+                    </div>
                 </div>
             )) }
         </div>
